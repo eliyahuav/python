ac40f9f34a83b69d0de78e3db7a018dd881665ad
Eliyahu Aviya <eliyahuav@edu.hac.ac.il>
2023-12-17 00:32:55 +0200

    8.2.1

---
+++ 8.2.1.py
@@ -0,0 +1,9 @@
+data = ("self", "py", 1.543)
+format_string = "Hello %s.%s learner, you have only %.1f units left before you master the course!"
+# s = string
+# f = float
+# % = format
+# %.Xf = X number after point
+
+
+print(format_string % data)
